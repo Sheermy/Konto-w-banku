4b7e5ab1d76092f439cdb0ff80a49f8f382cb638
Sheermy <dopa9669@gmail.com>
2026-04-03 16:40:10 +0200

    Poprawa Konto.cs i testy jednostkowe. [Krok 1]

--- Bank/Bank.Tests/UnitTest1.cs
+++ Bank/Bank.Tests/UnitTest1.cs
@@ -1,11 +1,54 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bank;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace Bank.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class KontoTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Wplata_Poprawna()
         {
+            var konto = new Konto("Uzytkownik", 100);
+            konto.Wplata(50);
+            Assert.AreEqual(150, konto.Bilans);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Test_Wplata_Zablokowane()
+        {
+            var konto = new Konto("Uzytkownik", 100);
+            konto.BlokujKonto();
+            konto.Wplata(50);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Test_Wyplata_BrakSrodkow()
+        {
+            var konto = new Konto("Uzytkownik", 100);
+            konto.Wyplata(200);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Test_Wplata_Ujemna()
+        {
+            var konto = new Konto("Uzytkownik", 100);
+            konto.Wplata(-10);
+        }
+
+        [TestMethod]
+        public void Test_Blokada_Status()
+        {
+            var konto = new Konto("Uzytkownik");
+            konto.BlokujKonto();
+            Assert.IsTrue(konto.Zablokowane);
+            konto.OdblokujKonto();
+            Assert.IsFalse(konto.Zablokowane);
         }
     }
 }
--- Bank/Bank/Konto.cs
+++ Bank/Bank/Konto.cs
@@ -1,23 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bank
 {
     public class Konto
     {
         private string klient;
-        protected decimal bilans; // protected, by klasy pochodne widziały stan
-        protected bool zablokowane = false;
+        private decimal bilans;
+        private bool zablokowane = false;
 
-        // Konstruktor domyślny prywatny - blokuje tworzenie pustych kont
         private Konto() { }
 
         public Konto(string klient, decimal bilansNaStart = 0)
         {
-            if (string.IsNullOrWhiteSpace(klient)) throw new ArgumentException("Nazwa klienta nie może być pusta.");
+            if (string.IsNullOrWhiteSpace(klient)) throw new ArgumentException();
             this.klient = klient;
             this.bilans = bilansNaStart;
         }
@@ -26,18 +21,18 @@ namespace Bank
         public decimal Bilans => bilans;
         public bool Zablokowane => zablokowane;
 
-        public virtual void Wplata(decimal kwota)
+        public void Wplata(decimal kwota)
         {
-            if (zablokowane) throw new InvalidOperationException("Konto zablokowane.");
-            if (kwota <= 0) throw new ArgumentOutOfRangeException(nameof(kwota), "Kwota musi być dodatnia.");
+            if (zablokowane) throw new InvalidOperationException();
+            if (kwota <= 0) throw new ArgumentOutOfRangeException(nameof(kwota));
             bilans += kwota;
         }
 
-        public virtual void Wyplata(decimal kwota)
+        public void Wyplata(decimal kwota)
         {
-            if (zablokowane) throw new InvalidOperationException("Konto zablokowane.");
-            if (kwota <= 0) throw new ArgumentOutOfRangeException(nameof(kwota), "Kwota musi być dodatnia.");
-            if (kwota > bilans) throw new InvalidOperationException("Brak środków.");
+            if (zablokowane) throw new InvalidOperationException();
+            if (kwota <= 0) throw new ArgumentOutOfRangeException(nameof(kwota));
+            if (kwota > bilans) throw new InvalidOperationException();
             bilans -= kwota;
         }
 
